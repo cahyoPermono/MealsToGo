53e09db4acd8cfe30fc6a300539b02f42536c223
Cahyo <cahyo.adi.permono@gmail.com>
2021-06-20 21:17:12 +0700

    adding infrastructure theme and apply it in project

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -3,16 +3,17 @@ import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 
 const Title = styled.Text`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 const RestauranCard = styled(Card)`
-  background-color: white;
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const RestauranCardCover = styled(Card.Cover)`
   background-color: white;
-  padding: 20px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaturantInfoCard = ({ restaurant = {} }) => {
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -6,15 +6,15 @@ import { RestaturantInfoCard } from "../components/restaurant-info-card.componen
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchContainer = styled(View)`
-  padding: 5px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 const RestaurantListContainer = styled(View)`
   flex: 1;
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantsScreen = () => {
